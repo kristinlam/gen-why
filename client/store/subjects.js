d22321e6b413b149a23fef545085a0b5538ee6a8
Kristin Lam <klam623@gmail.com>
2022-06-25 01:11:18 -0400

    added redux store for subjects

---
+++ client/store/subjects.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const GET_SUBJECTS = 'GET_SUBJECTS';
+
+const _getSubjects = (subjects) => {
+  return {
+    type: GET_SUBJECTS,
+    subjects,
+  };
+};
+
+export const getSubjects = () => {
+  return async (dispatch) => {
+    const { data: subjects } = await axios.get('/api/subjects');
+    dispatch(_getSubjects(subjects));
+  };
+};
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case SET_PROJECTS:
+      return action.subjects;
+    default:
+      return state;
+  }
+}
