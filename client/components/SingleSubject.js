2a7ee6941435dec0802e5616ed0b572918791987
Kristin Lam <klam623@gmail.com>
2022-06-26 14:09:29 -0400

    added security for links going to cross-origin destinations

--- client/components/SingleSubject.js
+++ client/components/SingleSubject.js
@@ -24,6 +24,7 @@ export const SingleSubject = (props) => {
               pathname: `${article.link}`,
             }}
             target="_blank"
+            rel="noreferrer"
           >
             {article.title}
           </Link>
