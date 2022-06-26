bd638280de7891ff0813f227105ad4621bfc51a7
Kristin Lam <klam623@gmail.com>
2022-06-25 22:19:55 -0400

    added reducer into combine reducers

--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,9 @@ import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import auth from './auth';
 import subjects from './subjects';
+import singleSubject from './singleSubject';
 
-const reducer = combineReducers({ auth, subjects });
+const reducer = combineReducers({ auth, subjects, singleSubject });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
