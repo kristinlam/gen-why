e53c7e82394bb0fa2071969ed5fd4847fd95d398
Kristin Lam <klam623@gmail.com>
2022-06-26 16:43:29 -0400

    created basic custom theme

--- client/App.js
+++ client/App.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Routes from './Routes';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#222222',
+    },
+    text: {
+      primary: '#ffffff',
+    },
+    link: {
+      textDecoration: 'none',
+      color: '#ffffff',
+    },
+  },
+});
+
+// const theme = createTheme({
+//   palette: {
+//     background: {
+//       default: '#222222',
+//     },
+//     text: {
+//       primary: '#ffffff',
+//     },
+//     // primary: {
+//     //   main: red[500],
+//     // },
+//     // secondary: {
+//     //   main: green[500],
+//     // },
+//   },
+// });
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
