4a30ef0bf8537cd8e08a6a2fb5a301563bbdbb6e
Kristin Lam <klam623@gmail.com>
2022-06-27 00:18:59 -0400

    changed text style for description on homepage

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import AllSubjects from './AllSubjects';
@@ -8,7 +6,7 @@ import AllSubjects from './AllSubjects';
 export const Home = () => {
   return (
     <Container>
-      <Typography variant="h6">
+      <Typography variant="body1">
         Millenials (Gen Y) have been blamed for ruining a lot of things. Golf.
         Cruise ships. Diamonds. The media was quick to point fingers at us for
         the cold-blooded murders of entire industries and timeless traditions,
@@ -17,9 +15,6 @@ export const Home = () => {
         Killed" headlines. As this era of brilliant journalism draws its final
         breath, let's look back on all the things millennials have been blamed
         for.
-        <Link component={RouterLink} to="/submit">
-          Are we missing anything?
-        </Link>
       </Typography>
       <AllSubjects />
     </Container>
