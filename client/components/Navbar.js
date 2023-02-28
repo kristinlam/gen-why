af29e1335472581ae416c2298a5b6e09d69105bd
Kristin Lam <klam623@gmail.com>
2023-02-28 18:37:14 -0500

    Refactor Navbar links for admin

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { logout } from '../store';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 
@@ -19,40 +18,44 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
       >
         <img className="logo" src="/logo.png" />
       </Link>
-      <Link
-        to="/submit"
-        component={RouterLink}
-        variant="body1"
-        underline="none"
-        color="#333"
-      >
-        Suggest a Story
-      </Link>
+
+      {isLoggedIn ? (
+        <div>
+          <Link
+            to="/home"
+            component={RouterLink}
+            variant="body1"
+            underline="none"
+            color="#333"
+          >
+            Admin Control
+          </Link>
+          <Link
+            onClick={handleClick}
+            to="#"
+            component={RouterLink}
+            variant="body1"
+            underline="none"
+            color="#333"
+          >
+            Log Out
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link
+            to="/submit"
+            component={RouterLink}
+            variant="body1"
+            underline="none"
+            color="#333"
+          >
+            Suggest a Story
+          </Link>
+        </div>
+      )}
     </Toolbar>
   </AppBar>
-
-  // <div>
-  //   <Link to="/">
-  //     <h1>Gen Why</h1>
-  //   </Link>
-  //   <nav>
-  //     {isLoggedIn ? (
-  //       <div>
-  //         {/* The navbar will show these links after you log in */}
-  //         <Link to="/home">Admin Control</Link>
-  //         <a href="#" onClick={handleClick}>
-  //           Logout
-  //         </a>
-  //       </div>
-  //     ) : (
-  //       <div>
-  //         {/* The navbar will show these links before you log in */}
-  //         {/* <Link to="/login">Login</Link>
-  //         <Link to="/signup">Sign Up</Link> */}
-  //       </div>
-  //     )}
-  //   </nav>
-  // </div>
 );
 
 const mapState = (state) => {
