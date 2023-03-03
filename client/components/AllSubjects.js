bbfb0d805d517499b21663648388311225d5f789
Kristin Lam <klam623@gmail.com>
2023-03-02 23:30:08 -0500

    Add animations on page load

--- client/components/AllSubjects.js
+++ client/components/AllSubjects.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getSubjects } from '../store/subjects';
+import { motion } from 'framer-motion';
 
 const AllSubjects = () => {
   const subjects = useSelector((state) => state.subjects);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getSubjects('approved'));
-  }, [dispatch]);
+  }, []);
 
   // sorting alphabetically
   const alphabeticalSort = (a, b) => {
@@ -25,9 +26,12 @@ const AllSubjects = () => {
     <div className="subject-list">
       <ul>
         {subjects.sort(alphabeticalSort).map((subject, index) => (
-          <li
+          <motion.li
             key={subject.id}
             className={`text-7xl mb-6 ${selectColor(index)}`}
+            initial={{ opacity: 0, translateY: 150 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
           >
             <a
               href={subject.link}
@@ -36,7 +40,7 @@ const AllSubjects = () => {
             >
               {subject.name}
             </a>
-          </li>
+          </motion.li>
         ))}
       </ul>
     </div>
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import AllSubjects from './AllSubjects';
 
 export const Home = () => {
   return (
     <div className="p-10 ">
       <AllSubjects />
-      <div className="fixed bottom-0 right-0 w-1/3 m-10 text-right">
+      <motion.div
+        className="fixed bottom-0 right-0 w-1/3 m-10 text-right"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+      >
         <h1 className="text-3xl font-bold mb-2.5 text-red ">Gen Why</h1>
         <p>
           Millenials have gotten a lot of flak from the media for ruining
@@ -19,7 +25,7 @@ export const Home = () => {
             Suggest a story.
           </Link>
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 };
