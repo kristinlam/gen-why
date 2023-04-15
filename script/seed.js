fa008e3931b558b2fcf87ca81b244afad9b7e109
Kristin Lam <klam623@gmail.com>
2023-04-15 17:48:54 -0400

    Remove comments to clean up code

--- script/seed.js
+++ script/seed.js
@@ -18,21 +18,14 @@ const subjectData = [
   },
 ];
 
-/**
- * seed - this function clears the database, updates tables to
- *      match the models, and populates the database.
- */
-
 async function seed() {
-  await db.sync({ force: true }); // clears db and matches models to tables
+  await db.sync({ force: true });
   console.log('db synced!');
 
-  // Create Users
   const users = await Promise.all([
     User.create({ username: 'kristin', password: '123', admin: true }),
   ]);
 
-  // Create Subjects
   const subjects = await Promise.all(
     subjectData.map((subject) => {
       return Subject.create(subject);
@@ -41,21 +34,9 @@ async function seed() {
 
   console.log(`seeded ${users.length} users`);
   console.log(`seeded ${subjects.length} subjects`);
-
   console.log(`seeded successfully`);
-  // return {
-  //   users: {
-  //     cody: users[0],
-  //     murphy: users[1],
-  //   },
-  // };
 }
 
-/*
- We've separated the `seed` function from the `runSeed` function.
- This way we can isolate the error handling and exit trapping.
- The `seed` function is concerned only with modifying the database.
-*/
 async function runSeed() {
   console.log('seeding...');
   try {
@@ -70,14 +51,8 @@ async function runSeed() {
   }
 }
 
-/*
-  Execute the `seed` function, IF we ran this module directly (`node seed`).
-  `Async` functions always return a promise, so we can use `catch` to handle
-  any errors that might occur inside of `seed`.
-*/
 if (module === require.main) {
   runSeed();
 }
 
-// we export the seed function for testing purposes (see `./seed.spec.js`)
 module.exports = seed;
