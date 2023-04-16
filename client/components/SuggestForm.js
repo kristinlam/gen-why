43fcd03fb8efe298595a873bf3460358162962d6
Kristin Lam <klam623@gmail.com>
2023-04-16 15:58:34 -0400

    Style form for submitting suggestions

--- client/components/SuggestForm.js
+++ client/components/SuggestForm.js
@@ -12,48 +12,52 @@ const SuggestForm = () => {
   };
   const [formValues, setFormValues] = useState(defaultValues);
 
-  const handleChange = (event) => {
-    setFormValues({ ...formValues, [event.target.name]: event.target.value });
+  const handleChange = (evt) => {
+    setFormValues({ ...formValues, [evt.target.name]: evt.target.value });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
     dispatch(createSubject(formValues));
     setFormValues(defaultValues);
   };
 
   return (
-    <div className="flex items-center	 justify-center h-screen text-2xl">
-      <div className="w-1/2 flex flex-col border-green border-4 rounded-md p-10">
-        <p className="mb-6">
-          Are we missing any of your favorite, most absurd stories? Let's fix
-          that.
-        </p>
+    <div className="px-6 flex items-center justify-center h-screen">
+      <div className="bg-white w-screen md:w-1/2 lg:w-1/3 flex flex-col rounded-3xl py-14 px-12">
+        <div className="text-center mb-8">
+          <h1 className=" mb-2">Suggest an Article</h1>
+          <p>
+            Are we missing any of your favorite, most absurd articles? Let's fix
+            that.
+          </p>
+        </div>
 
-        <form className="flex flex-col mb-4" onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <input
-              className="mb-2 w-full border border-green border-2 p-2 rounded-md focus:outline-none focus:border-yellow"
-              name="name"
-              placeholder="Name"
-              type="text"
-              value={formValues.name}
-              onChange={handleChange}
-            />
-            <input
-              className="border w-full border-green border-2 p-2 rounded-md focus:outline-none focus:border-yellow"
-              name="link"
-              placeholder="Link"
-              type="text"
-              value={formValues.link}
-              onChange={handleChange}
-            />
-          </div>
-          <button className="block bg-green text-cream p-2 rounded-lg">
+        <form className="flex flex-col mb-8" onSubmit={handleSubmit}>
+          <input
+            className="w-full mb-3 border-2 border-gray p-3 rounded-md focus:outline-none focus:border-black"
+            name="name"
+            placeholder="Topic"
+            type="text"
+            value={formValues.name}
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-3 border-2 border-gray p-3 rounded-md focus:outline-none focus:border-black"
+            name="link"
+            placeholder="Link"
+            type="text"
+            value={formValues.link}
+            onChange={handleChange}
+          />
+          <button
+            className="bg-yellow rounded-lg block w-full p-3 font-medium"
+            type="submit"
+          >
             Submit
           </button>
         </form>
-        <Link to="/" className="text-xl text-right">
+        <Link to="/" className="text-right">
           Go back
         </Link>
       </div>
