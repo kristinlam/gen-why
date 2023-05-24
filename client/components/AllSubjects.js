a50173cfcf83d06961c495d8f667484f0b753253
Kristin Lam <klam623@gmail.com>
2023-05-24 15:33:06 -0400

    Add animated loading message

--- client/components/AllSubjects.js
+++ client/components/AllSubjects.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getSubjects } from '../store/subjects';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { sortAlphabetical, cycleColors } from '../helpers';
 
 const AllSubjects = () => {
   const subjects = useSelector((state) => state.subjects);
+  const [isLoaded, setIsLoaded] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getSubjects('approved'));
+    dispatch(getSubjects('approved')).then(() => {
+      setIsLoaded(true);
+    });
   }, []);
 
   // Changes to shallow copy of nested objects (such as objects within arrays) will cause changes to original objects.
@@ -18,27 +21,41 @@ const AllSubjects = () => {
 
   return (
     <div className="subject-list">
-      <ul className="text-5xl md:text-6xl xl:text-7xl">
-        {deepCopyOfSubjects.sort(sortAlphabetical).map((subject, index) => (
-          <motion.li
-            key={subject.id}
-            className={`mb-4 md:mb-5 xl:mb-6 ${cycleColors(index)}`}
-            initial={{ opacity: 0, translateY: 150 }}
-            animate={{ opacity: 1, translateY: 0 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
+      {!isLoaded ? (
+        <AnimatePresence>
+          <motion.p
+            className="text-5xl text-green"
+            key="loading"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ y: 150, opacity: 0 }}
           >
-            <a
-              href={subject.link}
-              className={`textShadow-${cycleColors(index)} text-${cycleColors(
-                index
-              )}`}
-              target="_blank"
+            Loading...
+          </motion.p>
+        </AnimatePresence>
+      ) : (
+        <ul className="text-5xl md:text-6xl xl:text-7xl">
+          {deepCopyOfSubjects.sort(sortAlphabetical).map((subject, index) => (
+            <motion.li
+              key={subject.id}
+              className={`mb-4 md:mb-5 xl:mb-6 ${cycleColors(index)}`}
+              initial={{ opacity: 0, translateY: 150 }}
+              animate={{ opacity: 1, translateY: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
             >
-              {subject.name}
-            </a>
-          </motion.li>
-        ))}
-      </ul>
+              <a
+                href={subject.link}
+                className={`textShadow-${cycleColors(index)} text-${cycleColors(
+                  index
+                )}`}
+                target="_blank"
+              >
+                {subject.name}
+              </a>
+            </motion.li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
